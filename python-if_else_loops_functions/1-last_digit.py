776a5832954998c0ddd41d02c2dbc0af7a2eb827
SabyJeany <sabyjeany@gmail.com>
2025-09-05 22:33:00 +0200

    The lasy digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,6 @@ string5 = 'and is greater than 5'
 string0 = 'and is 0 '
 string6 = 'and is less than 6 and not 0'
 ldigit = abs(number) % 10
-
 if ldigit > 5:
     print(f"{string1} {number} {string2} {ldigit} {string5}")
 elif ldigit == 0:
