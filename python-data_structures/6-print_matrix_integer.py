4a32bb58514e87dfcd73314ec0db2a7906b38d8d
SabyJeany <sabyjeany@gmail.com>
2025-09-11 13:14:49 +0200

    list of lists

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-if matrix is None:
-return
-for row in matrix:
-print(" ".join(str(x) for x in row))
+def print_matrix_integer(matrix=[[i]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != len(matrix[i]) - 1:
+                print("{:d}".format(matrix[i][j]), end=" ")
+            else:
+                print("{:d}".format(matrix[i][j]), end="")
+        print()
