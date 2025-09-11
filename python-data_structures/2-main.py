d59a395d60f62bda0eac72f6fb995d635b500b7f
SabyJeany <sabyjeany@gmail.com>
2025-09-11 12:14:29 +0200

    print list & main 2

---
+++ python-data_structures/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+replace_in_list = __import__('2-replace_in_list').replace_in_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 2
+new_element = 10
+print("Before:", my_list)
+replace_in_list(my_list, idx, new_element)
+print("After:", my_list)
