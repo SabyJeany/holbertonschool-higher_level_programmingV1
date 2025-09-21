1906672bbe244f0e79ddcc4edd3b710e399dcd7c
SabyJeany <sabyjeany@gmail.com>
2025-09-21 21:16:59 +0200

    size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" class Square"""
+
+
+class Square:
+    """Represents a square.
+
+    This class currently has no properties or methods.
+    It can be extended later to include specific functionalities
+    related to a square.
+    """
+    def __init__(self, size):
+        self.__size = size
+
+    def get_size(self):
+        return self.__size
+
+Rectangle = __import__('5-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area:{}-Perimeter:{}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+del my_rectangle
+
+try:
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
