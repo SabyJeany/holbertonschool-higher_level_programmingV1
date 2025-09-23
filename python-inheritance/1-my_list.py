3ec45d488a0fd0b4581fa11338b75bf344b7d9f2
SabyJeany <sabyjeany@gmail.com>
2025-09-23 17:16:36 +0200

    Write a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task 1 : My List """
+
+
+class MyList(list):
+    """Create the class"""
+    def print_sorted(self):
+        """ Function that print a list of the instances of a given class """
+        sort_list = sorted(self)
+        print(sort_list)
