1fc4dd3f5992877fb3c5d6362db9503a56587a2a
SabyJeany <sabyjeany@gmail.com>
2025-09-25 12:04:20 +0200

    Write a class MyList that inherits from list

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+# 1-main.py
+
+MyList = __import__('1-my_list').MyList  # importer ta classe
 
 my_list = MyList()
 my_list.append(1)
@@ -7,6 +9,15 @@ my_list.append(4)
 my_list.append(2)
 my_list.append(3)
 my_list.append(5)
+
+print(my_list)
+my_list.print_sorted()
+print(my_list)
+
+my_list.append(10)
+my_list.append(8)
+my_list.append(6)
+
 print(my_list)
 my_list.print_sorted()
 print(my_list)
