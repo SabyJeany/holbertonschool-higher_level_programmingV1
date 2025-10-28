0eebc83f85067565156dfd6ffcf514589ab228a2
SabyJeany <sabyjeany@gmail.com>
2025-10-28 14:30:52 +0100

    a script that creates the database hbtn_02_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
+-- write a script that create the database hbtn_02_2 and the user user__0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_02_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_02_2.* TO 'user_0d_2'@'localhost';
