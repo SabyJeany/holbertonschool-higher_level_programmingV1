110376feda7589ee0beaf6f6e08afdb2e75fc208
SabyJeany <sabyjeany@gmail.com>
2025-10-30 16:04:33 +0100

    Number of shows genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
--- 13-count_shows_by_genre.sql
--- Lists all genres and the number of shows linked to each
--- Displays: tv_genres.name - number_of_shows
--- Results sorted by number_of_shows DESC, then tv_genres.name ASC
+--  Lists all genres and the number of shows linked to each
+--  Displays: tv_genres.name - number_of_shows
+--  Results sorted by number_of_shows DESC, then tv_genres.name ASC
 
 SELECT tv_genres.name AS name,
 COUNT(tv_show_genres.show_id) AS number_of_shows
