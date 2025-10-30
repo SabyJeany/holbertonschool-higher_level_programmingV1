1b9fe73daace7221af819183e51a2761be24b5e8
SabyJeany <sabyjeany@gmail.com>
2025-10-30 16:28:26 +0100

    tables contains only records where title dexter

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Lists all genre of the show Dexter in hbtn_0d_tvshows
+-- Results sorted by tv_genres.name ASC
+SELECT tv_genres.name
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_shows.title ASC;
