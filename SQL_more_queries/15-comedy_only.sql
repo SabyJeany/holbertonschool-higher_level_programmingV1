828487ace78e94e3c8a57a494b88ca0082f3513b
SabyJeany <sabyjeany@gmail.com>
2025-11-04 15:27:15 +0100

    git commit -m Update SQL queries and add 3 & 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,11 +2,14 @@
 -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
-SELECT tv_shows.title
+-- Lists all shows and their linked genres from the database hbtn_0d_tvshows
+-- If a show doesn't have a genre, display NULL
+-- Results sorted by tv_shows.title and tv_genres.name ASC
+
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title ASC;
+LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
